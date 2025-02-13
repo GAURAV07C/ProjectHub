9b4c5eb135eacb1d4ea4dc0186d787c3a1233124
Gaurav Kumar <gaurav989267@gmail.com>
2025-02-13 18:29:05 +0530

    Add unique userName field to User model and update GitHub authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
 import db from "./db/db";
 
-const adapter = PrismaAdapter(db);
+// const adapter = PrismaAdapter(db);
 
 async function testDB() {
   try {
@@ -19,7 +19,17 @@ testDB();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Github,
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
     Credentials({
       name: "Credentials",
       credentials: {
